4d9cf6fad606d023a62e83734388b4069e2d9b01
cobotz <cobotz87@gmail.com>
2017-04-30 02:47:31 +0800

    User reported fire filter

---
+++ RocketGPSMath/GPSMath/FireValidator.cs
@@ -0,0 +1,53 @@
+﻿using RocketGPS.Model;
+using RocketGPSMath.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketGPSMath.GPSMath
+{
+    public class FireValidator
+    {
+        public static double validationCheckRadiusKM = 0.5;
+        public static double validationAccuracy = 3;
+        public List<GPSCoordinateBearing> allUserReportedFire;
+
+        public bool IsUserReportedFireValid(GPSCoordinateBearing userReportedFire)
+        {
+            if (userReportedFire == null || userReportedFire == null)
+                return false;
+
+            int counter = 0;
+
+            foreach (var uf in allUserReportedFire)
+            {
+                if (uf == userReportedFire)
+                    continue;
+
+                if (userReportedFire.DistanceTo(uf) < validationCheckRadiusKM)
+                    counter++;
+
+                if (counter > validationAccuracy)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public List<GPSCoordinateBearing> FilterUserReportedFire()
+        {
+            List<GPSCoordinateBearing> validated = new List<GPSCoordinateBearing>();
+
+            foreach (var r in allUserReportedFire)
+            {
+                if(IsUserReportedFireValid(r))
+                    validated.Add(r);
+            }
+
+            return validated;
+        }
+
+    }
+}
