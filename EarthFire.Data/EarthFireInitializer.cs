b5df9ae904255d41326abff0739116de29008f3e
madz <madz>
2017-04-30 00:16:32 +0800

    add facade for fire source triangulation

---
+++ EarthFire.Data/EarthFireInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace EarthFire.Data
+{
+  public class EarthFireInitializer : IDatabaseInitializer<EarthFireContext>
+  {
+    public void InitializeDatabase(EarthFireContext context)
+    {
+      // TODO: Add seed data
+    }
+  }
+}
