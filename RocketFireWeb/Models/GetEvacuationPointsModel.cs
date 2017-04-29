4f76356df05c3fcd1d29c388ea96aa9467b1d4ce
arman <arman.ayadali@gemfive.com>
2017-04-29 16:30:50 +0100

    added comment for model

--- RocketFireWeb/Models/GetEvacuationPointsModel.cs
+++ RocketFireWeb/Models/GetEvacuationPointsModel.cs
@@ -3,5 +3,8 @@ using RocketFireWeb.Models;
 
 public class GetEvacuationPointsModel
 {
+  //TODO
+  // Model
+  // id, name, latitide, longitude, bearing, type ("fire", "evac", "station", "others")
   public IList<AddEvacuationPointModel> Locations { get; internal set; }
 }
